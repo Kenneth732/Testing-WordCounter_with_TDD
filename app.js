e097bb107d6f4d9eba54d6d6b058c3b9b1e997c2
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-05 13:40:04 +0300

    test

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function wordCounter(word){
+function wordCounter(text){
     let wordCounter = 0;
     const textArray = text.split("")
     textArray.forEach(function(word){
@@ -6,3 +6,15 @@ function wordCounter(word){
     })
     return wordCounter;
 }
+
+
+
+function wordCounter(text) {
+    let wordCounter = 0;
+    const textArray = text.split("");
+    textArray.forEach(function(element) {
+      wordCounter++;
+    });
+    return wordCounter;
+  }
+  
