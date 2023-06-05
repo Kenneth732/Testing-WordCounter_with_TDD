3971a5cbb3f58202f8c2865e996a9cd91e749ac3
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-05 15:41:54 +0300

    read...

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,4 @@
 // Business Logic
-
 function wordCounter(text) {
     if (text.trim().length === 0) {
         return 0;
@@ -15,10 +14,7 @@ function wordCounter(text) {
     return wordCounter;
 }
 
-// "word" refers to a specific word that you want to search for within a given text.
-// The parameter word is the value that represents the word you want to find occurrences of.
-// "text" refers to the actual text in which you want to search for the specified word. 
-// The parameter text is the value that represents the text you want to search within.
+
 function numberOfOccurrencesInText(word, text) {
     const textArray = text.split(" ");
     let wordCount = 0;
@@ -31,8 +27,4 @@ function numberOfOccurrencesInText(word, text) {
 }
 
 
-
-const word = 'hello';
-const text = 'Hello there, hello world!';
-const occurrences = numberOfOccurrencesInText(word, text);
-console.log(occurrences); // Output: 2
+// UI Logic
